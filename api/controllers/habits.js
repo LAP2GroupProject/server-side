eeca5e27f6df490c94c90c1ad0322796598deda8
johnpascual <ashleypascual@hotmail.es>
2022-08-16 14:56:25 +0100

    fixed docker

--- api/controllers/habits.js
+++ api/controllers/habits.js
@@ -1,4 +1,6 @@
 
+
+
 const Habit = require('../models/habits')
 
 
--- api/routes/habits.js
+++ api/routes/habits.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const habitsController = require("../controllers/habits");
 
-router.get('/', habitsController.index);
-router.get('/:id', habitsController.show);
-router.post('/', habitsController.create);
-router.patch('/:id', habitsController.patch)
+// router.get('/', habitsController.index);
+// router.get('/:id', habitsController.show);
+// router.post('/', habitsController.create);
+// router.patch('/:id', habitsController.patch)
 
 
 router.get('/', habitsController.habitIndex);
 router.post('/', habitsController.create);
 
+module.exports = router
 
 
