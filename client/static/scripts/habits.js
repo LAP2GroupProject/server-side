3f815ada0ffd30b999a7d7d45d756fc94ecf310b
johnpascual <ashleypascual@hotmail.es>
2022-08-16 11:10:40 +0100

    finished create habit flow

--- client/static/scripts/habits.js
+++ client/static/scripts/habits.js
@@ -1,3 +1,12 @@
+//// FREQUENCY SECTIONS ////
+let sleepFrequencySection = document.getElementById("habitSleepContainer")
+let waterFrequencySection = document.getElementById("habitWaterContainer")
+let exerciseFrequencySection = document.getElementById("habitExerciseContainer")
+let overviewSection = document.getElementById("displayHabitAndFrequencyBg")
+let overviewHabit = document.getElementById("overviewHabit")
+let overviewFrequency = document.getElementById("overviewFrequency")
+
+//// HIDE CREATE SECTION AND SHOW HABITS SECTION ////
 let noHabitsSection = document.getElementById("noHabitsBg")
 let createHabitButton = document.getElementById("noHabitBtn")
 let selectHabitSection = document.getElementById("habitsBg")
@@ -6,10 +15,47 @@ createHabitButton.addEventListener("click", () => {
     selectHabitSection.style.display = "block"
 })
 
+
 let selectHabitButtons = document.querySelectorAll(".selectedHabit")
 selectHabitButtons.forEach((habit) => {
     habit.addEventListener("click", () => {
         console.log(habit.value)
+        showFrequencySection(habit.value)
     })
 })
+
+
+function showFrequencySection(habit) {
+    selectHabitSection.style.display = "none"
+    if(habit === "Sleep") {
+        sleepFrequencySection.style.display = "block"
+        saveSelected(sleepFrequencySection, habit.toLowerCase())
+    } else if (habit === "Water") {
+        waterFrequencySection.style.display = "block"
+        saveSelected(waterFrequencySection, habit.toLowerCase())
+    } else if (habit === "Exercise") {
+        exerciseFrequencySection.style.display = "block"
+        saveSelected(exerciseFrequencySection, habit.toLowerCase())
+    }
+
+}
+
+function saveSelected (section, habit) {
+    let frequencyInput = document.getElementById(`${habit}Input`)
+    let frequencyButton = document.getElementById(`${habit}`)
+
+    frequencyButton.addEventListener("click", () => {
+        console.log(frequencyInput.value)
+        showOverview(section, habit, frequencyInput.value)
+    })
+}
+
+function showOverview (section, habit, frequency) {
+    section.style.display = "none"
+    overviewSection.style.display = "block"
+    overviewHabit.textContent = `[${habit}]`
+    overviewFrequency.textContent = `[${frequency}]`
+}
+
     
+
