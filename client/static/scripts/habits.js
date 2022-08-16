1d1ac6b1af28145e5501eb416072e3c36788be4a
vivian <vivian.v18723@gmail.com>
2022-08-16 14:34:32 +0100

    add tests for client side

--- client/static/scripts/habits.js
+++ client/static/scripts/habits.js
@@ -1,15 +1,21 @@
-let noHabitsSection = document.getElementById("noHabitsBg")
-let createHabitButton = document.getElementById("noHabitBtn")
-let selectHabitSection = document.getElementById("habitsBg")
+let noHabitsSection = document.getElementById("noHabitsBg");
+let createHabitButton = document.getElementById("noHabitBtn");
+let selectHabitSection = document.getElementById("habitsBg");
 createHabitButton.addEventListener("click", () => {
-    noHabitsSection.style.display = "none"
-    selectHabitSection.style.display = "block"
-})
+  noHabitsSection.style.display = "none";
+  selectHabitSection.style.display = "block";
+});
 
-let selectHabitButtons = document.querySelectorAll(".selectedHabit")
+let selectHabitButtons = document.querySelectorAll(".selectedHabit");
 selectHabitButtons.forEach((habit) => {
-    habit.addEventListener("click", () => {
-        console.log(habit.value)
-    })
-})
-    
+  habit.addEventListener("click", () => {
+    console.log(habit.value);
+  });
+});
+
+module.exports = {
+  noHabitsSection,
+  createHabitButton,
+  selectHabitSection,
+  selectHabitButtons
+};
