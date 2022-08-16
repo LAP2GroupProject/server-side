c3bc91fd0bb8e5b1b4636240fe1e56fa89bdedb6
CallumCheng <callumcheng@outlook.com>
2022-08-16 10:09:34 +0100

    merge conflicts

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const usersController = require('../controllers/users');
 
-router.get('/', usersController.index); //gets all users
+
 router.get('/:id', usersController.show); //gets all users by id
 router.post("/", verifyToken, usersController.create); //creates a post route for user
 
 
-module.exports = router;
+
