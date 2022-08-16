32a274280944e4f2e76d71971f4bbd73c0c9fb4e
johnpascual <ashleypascual@hotmail.es>
2022-08-16 14:44:50 +0100

    asdwasd

--- api/routes/habits.js
+++ api/routes/habits.js
@@ -7,11 +7,9 @@ router.get('/:id', habitsController.show);
 router.post('/', habitsController.create);
 router.patch('/:id', habitsController.patch)
 
-<<<<<<< HEAD
-router.delete('/:id', habitsController.destroy);
-=======
+
 router.get('/', habitsController.habitIndex);
 router.post('/', habitsController.create);
 
->>>>>>> 3b36581195eb0dba6132cbfeb9403d681b3d30d5
+
 
