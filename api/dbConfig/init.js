bf406390bd20654b9f8bac07c07d909a871a19b7
CallumCheng <callumcheng@outlook.com>
2022-08-17 17:30:58 +0100

    add env files

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -1,5 +1,12 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+    PGUSER: 'dfcohocmhpzmmf',
+    PGHOST: 'ec2-34-252-216-149.eu-west-1.compute.amazonaws.com',
+    PGDATABASE: 'dccrt91mtusg2s',
+    PGPASSWORD: '01dcaa0f2c797be037b8974ea159b624f06cae909f3da3f276a855d1338b6f93',
+    PGPORT: 5432,
+  })
+
 
 module.exports = pool;
