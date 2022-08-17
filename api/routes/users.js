dd39c945a2bcb1149c1b4a3ce008782a5c0c474c
johnpascual <ashleypascual@hotmail.es>
2022-08-17 22:20:26 +0100

    uncomment verifytoken

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,10 +1,10 @@
 const { Router } = require("express")
 
 const usersController = require('../controllers/users');
-//const verifyToken = require("../middleware/verifyToken");
+const verifyToken = require("../middleware/verifyToken");
 
 const usersRoutes = Router()
-//usersRoutes.use(verifyToken)
+usersRoutes.use(verifyToken)
 
 usersRoutes.get('/', usersController.index); //gets all users
 //usersRoutes.get('/:id', usersController.show); //gets all users by id
